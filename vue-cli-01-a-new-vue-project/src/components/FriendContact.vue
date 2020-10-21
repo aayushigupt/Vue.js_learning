b7d51b54d382b713d28c8fa3b7db13a8c4d460bd
aayushigupt <aayushigupta108@gmail.com>
2020-10-21 14:25:58 +0530

    Working on VueCLI

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ dataVisibility ? 'Hide' : 'Show '}} Details</button>
+    <ul v-if="dataVisibility">
+      <li>{{ friend.phone }}</li>
+      <li>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+//This is the Configuration Object
+export default {
+  data() {
+    return {
+      dataVisibility: false,
+      friend: {
+        id: "aayushi",
+        name: "Aayushi Gupta",
+        phone: "6377505602",
+        email: "aayushigupta108@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.dataVisibility = !this.dataVisibility;
+    },
+  },
+};
+</script>
