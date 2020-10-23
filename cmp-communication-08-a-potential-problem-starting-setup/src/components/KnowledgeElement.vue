7b61c179325f89d4a0951a16a216932f8fbb7403
aayushigupt <aayushigupta108@gmail.com>
2020-10-23 23:53:30 +0530

    Worked on provide and inject

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeElement.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="submitTopic(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  inject: ['submitTopic'],
+  props: ['id', 'topicName', 'description'],
+  emits: ['selectTopic'],
+};
+</script>
