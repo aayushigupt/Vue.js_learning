384d9147cac335834bded51698f8e5d7c895adbd
aayushigupt <aayushigupta108@gmail.com>
2020-10-29 16:03:02 +0530

    Added some custom tags and about the dynamic components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -12,24 +12,38 @@
      <h2>   {{ slotProps.item }} </h2>
       <p> {{ slotProps['another-prop']  }} </p>
       
-   </course-goals>
+   </course-goals> -->
+<button @click="selectComponent('active-goals')"> Active Goals </button>
+<button @click="selectComponent('manage-goals')"> Manage Goals </button>
+   <!-- <active-goals v-if="selectedComponent==='active-goals'"> </active-goals>
+   <manage-goals v-if="selectedComponent==='manage-goals'"> </manage-goals> -->
+<!-- dynamic component -->
+<!-- keep alive does not change the state -->
+<keep-alive> 
+<component :is="selectedComponent"> </component> 
+</keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 export default {
   components: {
     'the-header': TheHeader,
-    'badge-list': BadgeList,
-    'user-info': UserInfo,
-    'course-goals': CourseGoals
+    // 'badge-list': BadgeList,
+    // 'user-info': UserInfo,
+    // 'course-goals': CourseGoals,
+    'active-goals': ActiveGoals,
+    'manage-goals': ManageGoals
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -37,6 +51,11 @@ export default {
       },
     };
   },
+  methods: {
+    selectComponent(cmp){
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
---
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,37 @@
+<template>
+    <h2> Manage Goals </h2>
+    <input type="text" ref="goal"/> 
+    <button @click="submitGoal"> Submit </button>
+    <teleport to="body">
+    <error-alert v-if="dataIsInvalid">
+        <h2> Alert </h2>
+    <p> Please add a valid input </p>
+      <button @click="anotherChance"> Okay </button>  
+     </error-alert>
+     </teleport>
+</template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+    components: {
+        'error-alert': ErrorAlert,
+    },
+    data(){
+        return{
+            dataIsInvalid: false,
+        }
+    },
+    methods: {
+        submitGoal(){
+            const enteredMessage = this.$refs.goal.value;
+            if(enteredMessage === ''){
+                this.dataIsInvalid = true;
+            }
+        },
+        anotherChance(){
+            this.dataIsInvalid = false;
+        }
+    }
+}
+</script>
