d48289119e566a4462ca649bc256f032e9d10e6c
aayushigupt <aayushigupta108@gmail.com>
2020-11-02 01:42:31 +0530

    Added a new project

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+<the-header title="RememberMe">
+</the-header>
+<stored-resources :resources="storedResources">
+
+</stored-resources>
+
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+export default {
+  components: {
+   'stored-resources': StoredResources,
+   'the-header': TheHeader
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-docs',
+          title: 'Official Guide',
+          description: 'The Official Vuejs description',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          description: 'Learn to Google',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
+
---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,21 @@
+<template>
+    <base-card> 
+        <base-button @click="setSelectedTab('stored-resources')"> Stored Resources </base-button>
+        <base-button @click="setSelectedTab('add-resource')"> Add Resources </base-button>
+    </base-card>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            selectedTab: 'stored-resources'
+        };
+    },
+    methods: {
+        setSelectedTab(tab){
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
